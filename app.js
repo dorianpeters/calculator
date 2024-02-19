7fdd86d6713f64be9c0b58ce32d401c35baec069
Dorian Peters <dpeters08@gmail.com>
2024-02-19 10:45:10 -0800

    write operate function with switch statement to select proper math function

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let firstNum, secondNum, operator;
 
 // Functions
+
+function operate (num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      add(num1, num2);
+    case '-':
+      subtract(num1, num2);
+    case '*':
+      multiply(num1, num2);
+    case '/':
+      divide(num1, num2);
+  }
+}
+
 function add(num1, num2) {
   return num1 + num2;	
 };
-
 
 function subtract(num1, num2) {
 	return num1 - num2;
