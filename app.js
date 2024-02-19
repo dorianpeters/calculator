efdfb7edb40b516484127c3af1616a081794da04
Dorian Peters <dpeters08@gmail.com>
2024-02-19 13:10:32 -0800

    Added event handler to calculater buttons and numbers mirror to display

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 let firstNum, secondNum, operator;
+let displayContent = "";
+
+let buttons = document.querySelector('.buttons');
+let display = document.querySelector('.display');
+
+buttons.addEventListener('click', (event) => {
+  let buttonPressed = event.target.textContent;
+  if (isNaN(buttonPressed)) { // It's not a number
+
+  } else { // It is a number
+    displayContent += event.target.textContent;
+    display.textContent = displayContent;
+  }
+});
 
 // Functions
 
@@ -17,7 +31,7 @@ function operate (num1, operator, num2) {
 
 function add(num1, num2) {
   return num1 + num2;	
-};
+}
 
 function subtract(num1, num2) {
 	return num1 - num2;
@@ -30,3 +44,9 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1 / num2;
 }
+
+// Notes:
+// Click number -> display on screen -> appended to a string
+// Click operator -> store that, string is coverted to a number
+// click number -> blanks out the display, start new string.
+// Click operator or equals ->  convert num2 from display to number, do calculation, dispaly answer, num1
