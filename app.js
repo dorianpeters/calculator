d93da13c54a28817d4d55a198fa9a28c910e95c6
Dorian Peters <dpeters08@gmail.com>
2024-02-21 10:23:33 -0800

    remove math functions, check for divide by zero, make isNum helper, and isOperator helper, replace switch with if then.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-let firstNum = 0;
-let secondNum = 0;
-let operator;
-let hiddenNum;
+let storedNum = 0;
+let activeNum = 0;
+let operator = "=";
 let priorButtonOperator;
 
 const buttons = document.querySelector(".buttons");
@@ -10,40 +9,24 @@ const display = document.querySelector(".display");
 buttons.addEventListener("click", (event) => {
   const buttonPressed = event.target.textContent;
 
-  // isNaN type coersion behavior is intentional as numbers are in string format.
-  switch (buttonPressed) {
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-    case "0":
-      if (priorButtonOperator) {
-        display.textContent = "";
-        priorButtonOperator = false;
-      }
-      display.textContent += event.target.textContent;
-      break;
-    case "C":
-      clearAll();
-      break;
-    case "+":
-    case "-":
-    case "*":
-    case "/":
-    case "=":
-      secondNum = Number(display.textContent);
-      priorButtonOperator = true;
-      if (buttonPressed !== "=") operator = buttonPressed;
-      display.textContent = operate(firstNum, operator, secondNum);
-      firstNum = Number(display.textContent);
-      break;
-    default:
-      alert("case default");
+  if (isNum(buttonPressed)) {
+    if (priorButtonOperator) {
+      display.textContent = "";
+      priorButtonOperator = false;
+    }
+    display.textContent += event.target.textContent;
+  } else if (isOperator(buttonPressed)) {
+    activeNum = Number(display.textContent);
+    priorButtonOperator = true;
+    operator = buttonPressed;
+    display.textContent = operate(storedNum, operator, activeNum);
+    firstNum = Number(display.textContent);
+  } else if (buttonPressed === "=") {
+    alert("equals");
+  } else if (buttonPressed === "C") {
+    clearAll();
+  } else {
+    alert("case default");
   }
 });
 
@@ -52,35 +35,19 @@ function operate(num1, operator, num2) {
   let result = 0;
   switch (operator) {
     case "+":
-      result = add(num1, num2);
+      result = num1 + num2;
       break;
     case "-":
-      result = subtract(num1, num2);
+      result = num1 - num2;
       break;
     case "*":
-      result = multiply(num1, num2);
+      result = num1 * num2;
       break;
     case "/":
-      result = divide(num1, num2);
+      if (num2 !== 0) result = num1 / num2;
       break;
   }
   return result;
-}
-
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
 }
 
 function clearAll() {
@@ -89,4 +56,12 @@ function clearAll() {
   tempNum = 0;
   operator = "";
   display.textContent = "";
+}
+
+function isNum(num) {
+  return !isNan(num);
+}
+
+function isOperator(char) {
+  return ["+", "-", "*", "/"].includes(char);
 }
